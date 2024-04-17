46e469dfa8086450af434fa434d7346e54dd5e6a
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-17 18:32:12 +0200

    unique link fix

--- input/dailybuffer.py
+++ input/dailybuffer.py
@@ -2,8 +2,8 @@ import feedparser
 import datetime
 
 
-def get_feed_items(input_feed_link):
-    input_feed = feedparser.parse(input_feed_link)
+def get_feed_items(feed_link, logger):
+    input_feed = feedparser.parse(feed_link)
     feed_entries = input_feed['entries']
     feed_entries.reverse()
 
@@ -15,23 +15,30 @@ def get_feed_items(input_feed_link):
         agg_date = str(datetime.date(publish_date.tm_year, publish_date.tm_mon, publish_date.tm_mday))
         
         entry_content = entry['summary_detail']['value']
+        entry_link = entry['link']
         
-        dateagg_dict.setdefault(agg_date, []).append(entry_content)
+        dateagg_dict.setdefault(agg_date, []).append((entry_content, entry_link))
+    
 
     for agg_date, entries in dateagg_dict.items():
+
+        all_entry_contents, all_entry_links = map(list, zip(*entries))
+        
+
         output_feed_item = {}
         aggregated_entries_html = "<html>" + \
                 " <head> <meta charset = \"UTF-8\">" + \
                 "<title>" + agg_date+ "</title>" + \
                 "</head> <body>" + \
-                "<hr>".join(entries) + \
+                "<hr>".join(all_entry_contents) + \
                 "</body> </html>"
-        
-        link = input_feed_link
+
+        first_entry_in_aggregate_link = all_entry_links[0]
+    
 
         output_feed_item['title'] = agg_date
         output_feed_item['description'] = aggregated_entries_html
-        output_feed_item['link'] = link
+        output_feed_item['link'] = first_entry_in_aggregate_link
 
         output_feed_entries.append(output_feed_item)
     
