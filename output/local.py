b235c534aab345a5429a43ba1c8801d306921aed
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-17 17:38:05 +0200

    path handling change

--- output/local.py
+++ output/local.py
@@ -1,3 +1,5 @@
-def write_feed(feed_xml, feed_file_path, logger):
-    with open(feed_file_path, 'w') as f:
+import os
+
+def write_feed(feed_xml, output_dir, file_name, logger):
+    with open(os.path.join(output_dir, file_name), 'w') as f:
         f.write(feed_xml)
