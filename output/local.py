df73a31b5f66b5753ac64187695e58f02b73a0c0
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-17 18:31:59 +0200

    encoding fix in local

--- output/local.py
+++ output/local.py
@@ -1,5 +1,5 @@
 import os
 
 def write_feed(feed_xml, output_dir, file_name, logger):
-    with open(os.path.join(output_dir, file_name), 'w') as f:
+    with open(os.path.join(output_dir, file_name), encoding='utf8', mode='w') as f:
         f.write(feed_xml)
