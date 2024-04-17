81a42310f4dbe50fcceb4d959eb4cfc782c58ecc
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-17 21:35:49 +0200

    fixed s3 ul

--- output/s3.py
+++ output/s3.py
@@ -4,6 +4,7 @@ def write_feed(feed_xml, s3_bucket, s3_key, aws_access_key_id, aws_secret_access
     if aws_access_key_id == '' and aws_secret_access_key == '' and endpoint_url == '':
         s3 = boto3.client('s3')
     else:
-        s3 = boto3.client('s3')
+
+        s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, endpoint_url=endpoint_url)
     
-    s3.Bucket(s3_bucket).put_object(Key=s3_key, Body=feed_xml, ContentType='application/xml')
+    s3.put_object(Key=s3_key, Body=feed_xml, Bucket=s3_bucket, ContentType='application/xml')
