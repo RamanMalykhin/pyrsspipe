bb8d1be360b5ad2c932417c735d1952280d81c76
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-17 14:55:37 +0200

    optional cred passing for s3 output

--- output/s3.py
+++ output/s3.py
@@ -1,5 +1,9 @@
 import boto3
 
-def write_feed(feed_xml, s3_bucket, s3_key, logger):
-    s3 = boto3.client('s3')
+def write_feed(feed_xml, s3_bucket, s3_key, aws_access_key_id, aws_secret_access_key, endpoint_url, logger):
+    if aws_access_key_id == '' and aws_secret_access_key == '' and endpoint_url == '':
+        s3 = boto3.client('s3')
+    else:
+        s3 = boto3.client('s3')
+    
     s3.Bucket(s3_bucket).put_object(Key=s3_key, Body=feed_xml, ContentType='application/xml')
