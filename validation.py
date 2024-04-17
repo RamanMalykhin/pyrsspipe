97397a7280fa6b0b078f75720375488c33e73f66
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-17 20:41:34 +0200

    validation refactor

--- validation.py
+++ validation.py
@@ -1,14 +1,16 @@
 def validate_feed_data(feed_data: dict):
-    if not feed_data.get('feed_language'):
-        raise ValueError('feed_data must contain language')
-    if not feed_data.get('feed_name'):
-        raise ValueError('feed_data must contain feed_name')
-    if not feed_data.get('feed_filename'):
-        raise ValueError('feed_data must contain feed_filename')
-    if not feed_data.get('items_data'):
-        raise ValueError('feed_data must contain items_data')
+    _validate_feed_data(feed_data)
+
     for item_data in feed_data['items_data']:
-        if not item_data.get('link'):
-            raise ValueError('item_data must contain link')
-        if not item_data.get('description'):
-            raise ValueError('item_data must contain description')
+        _validate_item_data(item_data)
+        
+
+def _validate_feed_data(feed_data):
+    for required in ['feed_language', 'feed_name', 'feed_filename', 'items_data']:
+        if required not in feed_data:
+            raise ValueError(f'feed_data must contain {required}. feed_data is {feed_data}')
+        
+def _validate_item_data(item_data):
+    for required in ['link', 'description']:
+        if required not in item_data:
+            raise ValueError(f'item_data must contain {required}. item_data is {item_data}')
