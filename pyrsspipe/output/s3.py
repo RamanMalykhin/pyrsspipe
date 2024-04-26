70b2c2014ac2d754d93213fe9c0950f41f30a0db
RamanMalykhin <raman.malykhin@gmx.com>
2024-04-27 01:22:14 +0200

    version bump amd fix

--- pyrsspipe/output/s3.py
+++ pyrsspipe/output/s3.py
@@ -8,6 +8,7 @@ def write_feed(
     aws_access_key_id,
     aws_secret_access_key,
     endpoint_url,
+    acl,
     logger,
 ):
     if aws_access_key_id == "" and aws_secret_access_key == "" and endpoint_url == "":
@@ -21,6 +22,10 @@ def write_feed(
             endpoint_url=endpoint_url,
         )
 
-    s3.put_object(
-        Key=s3_key, Body=feed_xml, Bucket=s3_bucket, ContentType="application/xml"
+    response = s3.put_object(
+        Key=s3_key, Body=feed_xml, Bucket=s3_bucket, ContentType="application/xml", ACL = acl
+
     )
+    logger.info(f"boto3 put_object success. response: {response}")
+    logger.info(f"written feed to s3://{s3_bucket}/{s3_key}")
+
