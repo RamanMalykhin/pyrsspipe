e1dbc4b64530152021c9776d04bbb56e5373cd4a
RamanMalykhin <74513300+RamanMalykhin@users.noreply.github.com>
2024-10-26 05:58:23 +0200

    Add XPath input module for scraping (#7)
    * Add XPath input module for scraping
    
    Fixes #6
    
    
    
    * fixes
    
    ---------
    
    Co-authored-by: RamanMalykhin <raman.malykhin@gmx.com>

---
+++ pyrsspipe/input/xpath.py
@@ -0,0 +1,37 @@
+import lxml.html
+import requests
+import logging
+
+def get_feed_items(page_url, article_items_xpath, item_title_xpath, item_content_xpath, item_url_xpath, debug_mode, logger):
+    debug_mode = bool(debug_mode)
+    response = requests.get(page_url)
+    tree = lxml.html.fromstring(response.content)
+
+    feed_items = []
+    articles = tree.xpath(article_items_xpath)
+    
+    if debug_mode:
+        logger.setLevel(logging.DEBUG)
+
+    logger.debug(f"Scraped {len(articles)} items from {page_url}")
+    
+    for article in articles:
+        logger.debug(f"processing {article}")
+
+        title = str(article.xpath(item_title_xpath)[0])
+        logger.debug(f"found title: {title}")
+        content = str(article.xpath(item_content_xpath)[0])
+        logger.debug(f"found content: {content}")
+        url = str(article.xpath(item_url_xpath)[0])
+        logger.debug(f"found url: {url}")
+
+        feed_item = {}
+        feed_item["title"] = title
+        feed_item["description"] = content
+        feed_item["link"] = url
+        feed_items.append(feed_item)
+        
+    if debug_mode:
+        logger.setLevel(logging.INFO)
+
+    return feed_items
--- pyrsspipe/pyrsspipe.py
+++ pyrsspipe/pyrsspipe.py
@@ -15,36 +15,38 @@ def pyrsspipe():
     # Initialize logger
     logging.basicConfig(
         level=logging.INFO,
+        encoding='utf-8',
         format="%(asctime)s - %(levelname)s - %(message)s",
         handlers=[
             logging.FileHandler(os.path.join(log_dir, "pyrsspipe.log")),
             logging.StreamHandler(),
         ],
     )
+    logger = logging.getLogger()
 
     try:
         parser = argparse.ArgumentParser()
         parser.add_argument("--pipeconfig", help="Specify the pipeconfig name", required=True)
         args = parser.parse_args()
         pipeconfig_name = args.pipeconfig
-        logging.info(f"using pipeconfig {pipeconfig_name}")
+        logger.info(f"using pipeconfig {pipeconfig_name}")
 
         # Open the pipeconfig file
 
         pipeconfig_path = f"{pipeconfig_dir}/{pipeconfig_name}.json"
         with open(pipeconfig_path, "r") as file:
             pipeconfig = json.load(file)
-        logging.info(f"parsed pipeconfig {pipeconfig_name}")
+        logger.info(f"parsed pipeconfig {pipeconfig_name}")
 
         input_module_name = pipeconfig["input"]["module"]
         input_module = import_module(f"pyrsspipe.input.{input_module_name}")
         input_function = getattr(input_module, "get_feed_items")
 
-        logging.info(
+        logger.info(
             f"imported input module {input_module}, using input function {input_function}"
         )
 
-        feed_items = input_function(**pipeconfig["input"]["args"], logger=logging)
+        feed_items = input_function(**pipeconfig["input"]["args"], logger=logger)
 
         feed_data = {
             "feed_name": pipeconfig["feed_name"],
@@ -55,24 +57,24 @@ def pyrsspipe():
         try:
             validate_feed_data(feed_data)
         except ValueError as e:
-            logging.error(f"invalid feed_data: {e}")
+            logger.error(f"invalid feed_data: {e}")
             raise e
 
         feed_xml = make_feed_wrapper(feed_data)
-        logging.info("feed_xml created")
+        logger.info("feed_xml created")
 
         output_module_name = pipeconfig["output"]["module"]
         output_module = import_module(f"pyrsspipe.output.{output_module_name}")
         output_function = getattr(output_module, "write_feed")
-        logging.info(
+        logger.info(
             f"imported output module {output_module}, using output function {output_function}"
         )
 
-        output_function(feed_xml, **pipeconfig["output"]["args"], logger=logging)
-        logging.info("output complete")
+        output_function(feed_xml, **pipeconfig["output"]["args"], logger=logger)
+        logger.info("output complete")
 
-        logging.info("pyrsspipe complete")
+        logger.info("pyrsspipe complete")
 
     except Exception as e:
-        logging.error(e)
+        logger.error(e)
         raise e
