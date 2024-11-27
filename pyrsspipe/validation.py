3e75429eb5373ba406bd82b07b841942203c3350
RamanMalykhin <74513300+RamanMalykhin@users.noreply.github.com>
2024-11-27 12:45:55 +0100

    Finished validator

--- pyrsspipe/validation.py
+++ pyrsspipe/validation.py
@@ -1,4 +1,3 @@
-# validation.py
 
 from pydantic import BaseModel, validator, ValidationError
 import importlib
@@ -11,13 +10,38 @@ def import_module(module_name: str):
         raise ValidationError(f"Module '{module_name}' could not be imported")
 
 class ConfigModel(BaseModel):
-    input_module: str
-    output_module: str
+    feed_name: str
+    feed_language: str
+    input_module: dict
+    output_module: dict
 
-    @validator('input_module', 'output_module', pre=True)
+    @validator('input_module','output_module', pre=True)
     def validate_and_import_module(cls, value):
         module = import_module(value)
         callable = getattr(module, 'execute', None)
         if callable is None:
             raise ValidationError(f"Module '{value}' does not contain 'execute' method")
         cls.execute = callable
+    
+    @validator('input_module', 'output_module', pre=True)
+    def validate_args(cls, value):
+        mandatory = set()
+        optional = set()
+        
+        defined_args = inspect.signature(cls.execute).parameters
+        for arg in defined_args.values():
+            if arg.default == inspect._empty:
+                mandatory.add(arg.name)
+            else:
+                optional.add(arg.name)
+        
+        mandatory_missing = mandatory.difference(set(value['args'].keys()))
+        optional_missing = optional.difference(set(value['args'].keys()))
+        
+        if mandatory_missing:
+            raise ValueError(f'Missing mandatory arguments: {mandatory_missing}')
+        elif optional_missing:
+            for arg in optional_missing:
+                print(f"Optional argument {arg} missing. Default value {defined_args[arg].default} will be used")
+                
+        
